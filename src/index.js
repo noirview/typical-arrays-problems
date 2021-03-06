61bbce666c475d4e4b0f8477523b30168ec2d620
Anton Stepurko <stepurko.anton@gmail.com>
2021-03-06 22:53:50 +0300

    feat: implemented min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (arguments.length == 0) return 0;
+    if (array == 0) return 0;
+
+    let min = array[0];
+
+    for (let num of array) {
+        if (min > num) min = num;
+    }
+
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length == 0) return 0;
+    if (array.length == 0) return 0;
+
+    let max = array[0];
+
+    for (let num of array) {
+        if (max < num) max = num;
+    }
+
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length == 0) return 0;
+    if (array.length == 0) return 0;
+    
+    let sum = 0;
+
+    for (let num of array) {
+        sum += num;
+    }
+
+    return sum / array.length;
 }
